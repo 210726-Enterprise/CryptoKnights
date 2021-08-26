74d062e82a0a6ffd2d1a056b0d62a3901f892942
cptvictor <victorsyanwenliu19@gmail.com>
2021-08-26 15:53:36 -0400

    testing framework

---
+++ src/test/java/com/revature/controller/TransactionControllerTest.java
@@ -0,0 +1,31 @@
+package com.revature.controller;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TransactionControllerTest
+{
+
+    @BeforeEach
+    void setUp()
+    {
+    }
+
+    @AfterEach
+    void tearDown()
+    {
+    }
+
+    @Test
+    void getAllTransactions()
+    {
+    }
+
+    @Test
+    void createTransaction()
+    {
+    }
+}
