43272eed4a92885a674577e307408bbb10061802
Rufus1911 <jacob.brummett@revature.net>
2021-08-26 15:52:35 -0400

    Added User model, service, controller, and repository class/interface(s)

--- src/main/java/com/revature/project2/model/User.java
+++ src/main/java/com/revature/project2/model/User.java
@@ -1,2 +1,36 @@
-package com.revature.project2.model;public class User {
+package com.revature.project2.model;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name="users")
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class User {
+
+    @Id
+    @Column(name="user_id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int userId;
+
+    @Column(name="username", unique = true, nullable = false)
+    private String username;
+
+    @Column(name="password", nullable = false)
+    private String password;
+
+    @Column(name="first_name")
+    private String firstName;
+
+    @Column(name="last_name")
+    private String lastName;
+
+    @Column(name="email", unique = true)
+    private String email;
 }
