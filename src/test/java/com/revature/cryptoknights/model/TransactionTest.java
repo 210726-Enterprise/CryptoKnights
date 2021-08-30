1724861a1f640e45de78723e4c44957bec466fd2
cptvictor <victorsyanwenliu19@gmail.com>
2021-08-30 17:28:34 -0400

    unnecessary import

--- src/test/java/com/revature/cryptoknights/model/TransactionTest.java
+++ src/test/java/com/revature/cryptoknights/model/TransactionTest.java
@@ -6,7 +6,6 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.mock.mockito.MockBean;
 
-import javax.sound.sampled.Port;
 import java.time.LocalDateTime;
 
 import static org.junit.jupiter.api.Assertions.*;
