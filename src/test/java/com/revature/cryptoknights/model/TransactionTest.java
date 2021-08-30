a1e7306404144ba4c2cbb20c50e87355d0c31873
cptvictor <victorsyanwenliu19@gmail.com>
2021-08-30 12:22:21 -0400

    transaction model tests

--- src/test/java/com/revature/cryptoknights/model/TransactionTest.java
+++ src/test/java/com/revature/cryptoknights/model/TransactionTest.java
@@ -3,15 +3,41 @@ package com.revature.cryptoknights.model;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.mock.mockito.MockBean;
+
+import javax.sound.sampled.Port;
+import java.time.LocalDateTime;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class TransactionTest
 {
+    @Autowired
+    private Transaction testTransaction;
+
+    @MockBean
+    private Portfolio mockPortfolio1;
+
+    @MockBean
+    private Portfolio mockPortfolio2;
+
+    @MockBean
+    private LocalDateTime mockLocalDateTime1;
+
+    @MockBean
+    private LocalDateTime mockLocalDateTime2;
 
     @BeforeEach
     void setUp()
     {
+        testTransaction.setTransactionId(1);
+        testTransaction.setTransactionDateTime(mockLocalDateTime1);
+        testTransaction.setTransactionInCurrency("testCurrencyIn");
+        testTransaction.setTransactionInAmount(100);
+        testTransaction.setTransactionOutCurrency("testCurrencyOut");
+        testTransaction.setTransactionOutAmount(200);
+        testTransaction.setPortfolio(mockPortfolio1);
     }
 
     @AfterEach
@@ -22,70 +48,91 @@ class TransactionTest
     @Test
     void getTransactionId()
     {
+        assertEquals(testTransaction.getTransactionId(), 1);
     }
 
     @Test
     void getPortfolio()
     {
+        assertEquals(testTransaction.getPortfolio(), mockPortfolio1);
     }
 
     @Test
     void getTransactionDateTime()
     {
+        assertEquals(testTransaction.getTransactionDateTime(), mockLocalDateTime1);
     }
 
     @Test
     void getTransactionInCurrency()
     {
+        assertEquals(testTransaction.getTransactionInCurrency(), "testCurrencyIn");
     }
 
     @Test
     void getTransactionInAmount()
     {
+        assertEquals(testTransaction.getTransactionInAmount(), 100);
     }
 
     @Test
     void getTransactionOutCurrency()
     {
+        assertEquals(testTransaction.getTransactionOutCurrency(), "testCurrencyOut");
     }
 
     @Test
     void getTransactionOutAmount()
     {
+        assertEquals(testTransaction.getTransactionOutAmount(), 200);
     }
 
     @Test
     void setTransactionId()
     {
+        testTransaction.setTransactionId(2);
+        assertEquals(testTransaction.getTransactionId(), 2);
     }
 
     @Test
     void setPortfolio()
     {
+        testTransaction.setPortfolio(mockPortfolio2);
+        assertEquals(testTransaction.getPortfolio(), mockPortfolio2);
     }
 
     @Test
     void setTransactionDateTime()
     {
+        testTransaction.setTransactionDateTime(mockLocalDateTime2);
+        assertEquals(testTransaction.getTransactionDateTime(), mockLocalDateTime2);
     }
 
     @Test
     void setTransactionInCurrency()
     {
+        testTransaction.setTransactionInCurrency("testCurrencyIn2");
+        assertEquals(testTransaction.getTransactionInCurrency(), "testCurrencyIn2");
     }
 
     @Test
     void setTransactionInAmount()
     {
+        testTransaction.setTransactionInAmount(1000);
+        assertEquals(testTransaction.getTransactionInAmount(), 1000);
     }
 
     @Test
     void setTransactionOutCurrency()
     {
+        testTransaction.setTransactionOutCurrency("testCurrencyOut2");
+        assertEquals(testTransaction.getTransactionOutCurrency(), "testCurrencyOut2");
     }
 
     @Test
     void setTransactionOutAmount()
     {
+        testTransaction.setTransactionOutAmount(20);
+        assertEquals(testTransaction.getTransactionOutAmount(), 20);
     }
 }
