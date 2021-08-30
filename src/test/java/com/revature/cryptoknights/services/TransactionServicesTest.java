a07faec6204ec7e6abdd476dd21c6c569e9a776e
cptvictor <victorsyanwenliu19@gmail.com>
2021-08-30 17:28:15 -0400

    transaction services test

--- src/test/java/com/revature/cryptoknights/services/TransactionServicesTest.java
+++ src/test/java/com/revature/cryptoknights/services/TransactionServicesTest.java
@@ -1,17 +1,50 @@
 package com.revature.cryptoknights.services;
 
+import com.revature.cryptoknights.model.Portfolio;
+import com.revature.cryptoknights.model.Transaction;
+import com.revature.cryptoknights.repo.TransactionRepository;
+import com.revature.cryptoknights.service.TransactionServices;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.mock.mockito.MockBean;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.mockito.Mockito.when;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class TransactionServicesTest
 {
+    @Autowired
+    private TransactionServices transactionServicesTest;
+
+    @MockBean
+    private TransactionRepository mockTransactionRepository;
+
+    @MockBean
+    private LocalDateTime mockLocalDateTime;
+
+    private List<Transaction> transactionList;
+    private Transaction transaction;
 
     @BeforeEach
     void setUp()
     {
+        transaction.setTransactionId(1);
+        transaction.setTransactionDateTime(mockLocalDateTime);
+        transaction.setTransactionInCurrency("testCurrencyIn");
+        transaction.setTransactionInAmount(100);
+        transaction.setTransactionOutCurrency("testCurrencyOut");
+        transaction.setTransactionOutAmount(200);
+        transaction.setPortfolio(new Portfolio());
+
+        transactionList = new ArrayList<>();
+        transactionList.add(transaction);
     }
 
     @AfterEach
@@ -22,10 +55,14 @@ class TransactionServicesTest
     @Test
     void saveTransaction()
     {
+        when(mockTransactionRepository.save(transaction)).thenReturn(transaction);
+        assertEquals(transactionServicesTest.saveTransaction(transaction), transaction);
     }
 
     @Test
     void getAllTransactions()
     {
+        when(mockTransactionRepository.findAll()).thenReturn(transactionList);
+        assertEquals(transactionServicesTest.getAllTransactions(), transactionList);
     }
 }
