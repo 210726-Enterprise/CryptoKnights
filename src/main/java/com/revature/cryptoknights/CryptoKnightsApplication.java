c923c25eb5d70d7e77c127b71044ea5eb53dbf1f
AlwynZ <47126967+AlwynZ@users.noreply.github.com>
2021-09-06 14:42:27 -0400

    added swagger dependency

--- src/main/java/com/revature/cryptoknights/CryptoKnightsApplication.java
+++ src/main/java/com/revature/cryptoknights/CryptoKnightsApplication.java
@@ -2,8 +2,11 @@ package com.revature.cryptoknights;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
 
 @SpringBootApplication
+@EnableSwagger2
 public class CryptoKnightsApplication
 {
 	public static void main(String[] args) {
